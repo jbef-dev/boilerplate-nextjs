232884b12cd5813d72ab5686e32aef3d129a6398
Jorge Befan <jorge.befan@gmail.com>
2022-06-11 00:47:13 +0200

    adding comments to config

--- lang/config.ts
+++ lang/config.ts
@@ -5,6 +5,7 @@ type IntlMessageKeys = keyof typeof en
 const locales = ['en', 'es'] as const
 export type Locales = typeof locales[number]
 
+// strongly gyped ids for function and component as per docs:
 // https://formatjs.io/docs/react-intl/
 declare global {
   namespace FormatjsIntl {
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
+    // Locales provided in config following docs:
     // https://nextjs.org/docs/advanced-features/i18n-routing
     locales: ['en', 'es'],
     defaultLocale: 'es',
